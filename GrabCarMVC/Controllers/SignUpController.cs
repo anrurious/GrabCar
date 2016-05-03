35b17fb91ec5ee01ee407114f655d5db1f60c20f
anrurious <melitondebnath@gmail.com>
2016-05-03 16:31:06 +0600

    Added Files
    Added project Files

---
+++ GrabCarMVC/Controllers/SignUpController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrabCarMVC.Controllers
+{
+    public class SignUpController : Controller
+    {
+        //
+        // GET: /SignUp/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+        public ActionResult Final()
+        {
+            return View();
+        }
+        public ActionResult PassengerRegistration()
+        {
+            return View();
+        }
+	}
+}
---
+++ GrabCarMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GrabCarMVC.Startup))]
+namespace GrabCarMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
